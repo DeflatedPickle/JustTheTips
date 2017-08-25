508ff2624168ed3413c508950cfa19434ea6464a
DeflatedPickle <DeflatedPickle@gmail.com>
2017-08-25 03:18:46 +0100

    Updated versions

--- src/main/java/com/deflatedpickle/justthetips/Reference.java
+++ src/main/java/com/deflatedpickle/justthetips/Reference.java
@@ -4,8 +4,8 @@ public class Reference {
     public static final String MOD_ID = "justthetips";
     public static final String NAME = "JustTheTips";
     // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
-    public static final String VERSION = "1.11.2-1.0.1.1";
-    public static final String ACCEPTED_VERSIONS = "[1.11.2]";
+    public static final String VERSION = "1.10.2-1.0.1.1";
+    public static final String ACCEPTED_VERSIONS = "[1.10.2]";
 
     public static final String CLIENT_PROXY_CLASS = "com.deflatedpickle.justthetips.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.deflatedpickle.justthetips.proxy.ServerProxy";
